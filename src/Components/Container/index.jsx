5f7a4265bfe3bad14ad9452a685a12257d04a172
wwwacedo <eng.marianammacedo@gmail.com>
2023-07-06 06:32:25 -0300

    refatorando os elementos existentes com styled-components

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -1,14 +1,31 @@
+import styled from 'styled-components';
+
 import Titulo from "../Titulo";
 import Conta from "../Conta";
 
+const ContainerWrapper = styled.div`
+	background-color: #f1f1f1;
+  	min-height: 90vh;
+  	padding: 0px 15vw;
+`
+
+const Conteudo = styled.section`
+ 	display: flex;
+  	flex-direction: row;
+  	justify-content: space-between;
+	@media (max-width: 800px) {
+		flex-direction: column;
+	}
+` 
+
 const Container = () => {
   return (
-    <div className="container">
+    <ContainerWrapper>
       <Titulo>Olá Fulano!</Titulo>
-      <section className="conteudo">
+      <Conteudo>
         <Conta />
-      </section>
-    </div>
+      </Conteudo>
+    </ContainerWrapper>
   );
 };
 
