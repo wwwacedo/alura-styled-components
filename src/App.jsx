222e35207c20a74ebea0918ee1d141d1bca192c2
wwwacedo <eng.marianammacedo@gmail.com>
2023-07-06 05:32:36 -0300

    subindo o projeto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Cabecalho from "./Components/Cabecalho";
+import Container from "./Components/Container";
+import { GlobalStyle } from "./Components/GlobalStyle";
+
+function App() {
+	return (
+		<>
+			<GlobalStyle />
+			<Cabecalho />
+			<Container />
+		</>
+	);
+}
+
+export default App;
